0fdd1a12315c361124419cfc9684f39c4dd9fb5b
Lencybhalodiya <worklency@gmail.com>
2024-07-27 19:14:47 +0530

    winston and morgan config for logs

--- node-lib/index.js
+++ node-lib/index.js
@@ -10,13 +10,13 @@ app.use(
   morgan(morganFormat, {
     stream: {
       write: (message) => {
-        const logObject = {
+        const log = {
           method: message.split(" ")[0],
           url: message.split(" ")[1],
           status: message.split(" ")[2],
           responseTime: message.split(" ")[3],
         };
-        logger.info(JSON.stringify(logObject));
+        logger.info(JSON.stringify(log));
       },
     },
   })
